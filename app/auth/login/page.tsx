dd225746b841aa95456df24afcb50478b5cca948
Gentuar333-max <gentuar333@gmail.comgit>
2026-03-17 23:37:59 +0100

    remove auth login page

--- app/auth/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-
-const LoginContent = dynamic(() => import("@/components/LoginContent"), {
-  ssr: false,
-});
-
-export default function LoginPage() {
-  return <LoginContent />;
-}
